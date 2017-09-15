bc052462048b6c1c84c8effc65ccc7dca8d7c79f
Kevin Green <greenkev@oregonstate.edu>
2017-09-15 14:36:23 -0700

    Updated Lossy Animation code to be better encapsulated

--- Lossy Monoped/runSlipDynamics.m
+++ Lossy Monoped/runSlipDynamics.m
@@ -1,10 +1,10 @@
 % close all
 clear raibertController
 obj = SLIPdynamics();
-obj.dataTimeStep = 0.02;  %Output timestep (sec)
+obj.dataTimeStep = 0.01;  %Output timestep (sec)
 des_vel = 0.2; %m/s
 
-obj = simulate(obj,@raibertController,[0,11],des_vel);
+obj = simulate(obj,@raibertController,[0,10],des_vel);
 %%
 figure(9)
 subplot(2,1,1)
@@ -24,7 +24,6 @@ legend('Simulation','Desired');
 ylabel('Body Horizontal velocity (m/sec)');
 xlabel('time (sec)');
 %%
-% % close all
 aObj = monopedAnimation(obj);
 for i = 1:length(obj.t)
 %    aObj.dispAtIndex(i);
